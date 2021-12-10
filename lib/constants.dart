0be52acf520804c4e8fe3ca6ec24b8cc070da9b3
Anirudha0789 <anirudha0789@gmail.com>
2021-12-10 14:54:54 +0530

    UI changes

---
+++ lib/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  contentPadding: EdgeInsets.symmetric(
+    vertical: 10.0,
+    horizontal: 20.0,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.orangeAccent, width: 2.5),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.orangeAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+);
+
+const kAnimatedTextStyle = TextStyle(
+    fontSize: 35.0, fontWeight: FontWeight.w900, color: Colors.orange);
+
+
+
+//borderRadius: BorderRadius.all(Radius.circular(32.0)
